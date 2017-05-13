481326fad4a4a1c2f0b6896d8e21aa1cab8b1d1f
Maciej Hajduk <ebrithil97@gmail.com>
2017-05-13 19:38:25 +0200

    character MOVE

--- src/Character.java
+++ src/Character.java
@@ -1,7 +1,39 @@
-package PACKAGE_NAME;
-
-/**
- * Created by okkindel on 13.05.17.
- */
 public class Character {
+
+    protected static int x_value = 1;
+    protected static int y_value = 1;
+
+    public Character() {
+
+        Room.sizes[x_value][y_value] = 4;
+    }
+    public void increaseY() {
+        if (Room.sizes[x_value][y_value+1] != 2) {
+            y_value++;
+            Room.sizes[x_value][y_value] = 4;
+            Room.sizes[x_value][y_value - 1] = 1;
+        }
+    }
+    public void decreaseY() {
+        if (Room.sizes[x_value][y_value-1] != 2) {
+            y_value--;
+            Room.sizes[x_value][y_value] = 4;
+            Room.sizes[x_value][y_value + 1] = 1;
+        }
+    }
+    public void increaseX() {
+        if (Room.sizes[x_value+1][y_value] != 2) {
+            x_value++;
+            Room.sizes[x_value][y_value] = 4;
+            Room.sizes[x_value - 1][y_value] = 1;
+        }
+    }
+    public void decreaseX() {
+        if (Room.sizes[x_value-1][y_value] != 2) {
+            x_value--;
+            Room.sizes[x_value][y_value] = 4;
+            Room.sizes[x_value + 1][y_value] = 1;
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -9,10 +12,40 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Room room = new Room();
-        Corridor corridor = new Corridor();
+        //Corridor corridor = new Corridor();
+        Character character = new Character();
         Draftsman dungeon = new Draftsman();
+
+        dungeon.load();
         primaryStage.setTitle("Pixel Caves");
         sc = new Scene(dungeon.draw(), 800, 600);
+
+        sc.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.W) {
+                    character.decreaseY();
+                    sc.setRoot(dungeon.draw());
+                    primaryStage.setScene(sc);
+                }
+                if (event.getCode() == KeyCode.A) {
+                    character.decreaseX();
+                    sc.setRoot(dungeon.draw());
+                    primaryStage.setScene(sc);
+                }
+                if (event.getCode() == KeyCode.S) {
+                    character.increaseY();
+                    sc.setRoot(dungeon.draw());
+                    primaryStage.setScene(sc);
+                }
+                if (event.getCode() == KeyCode.D) {
+                    character.increaseX();
+                    sc.setRoot(dungeon.draw());
+                    primaryStage.setScene(sc);
+                }
+            }
+        });
+
         primaryStage.setScene(sc);
         primaryStage.show();
     }
