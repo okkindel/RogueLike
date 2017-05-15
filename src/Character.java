d8709294db7b52265a7c2d8acf528fb765e58e6b
Maciej Hajduk <ebrithil97@gmail.com>
2017-05-15 17:16:43 +0200

    NOW ITS MERGED

--- src/Character.java
+++ src/Character.java
@@ -1,30 +1,18 @@
 public class Character {
 
-<<<<<<< HEAD
     private static int x_value = 1;
     private static int y_value = 1;
     static int whereAmI = 0;
-=======
-    protected static int x_value = 1;
-    protected static int y_value = 1;
-    protected static int whereAmI = 0;
     protected int last_tile = 1;
->>>>>>> origin/maku
 
     Character() {
-
         Main.rooms.get(whereAmI).sizes[x_value][y_value] = 4;
     }
 
-<<<<<<< HEAD
     void increaseY() {
-        if (Main.rooms.get(whereAmI).sizes[x_value][y_value+1] == 1) {
-=======
-    public void increaseY() {
         if (Main.rooms.get(whereAmI).sizes[x_value][y_value+1] != 2) {
             Main.rooms.get(whereAmI).sizes[x_value][y_value ] = last_tile;
             last_tile = Main.rooms.get(whereAmI).sizes[x_value][y_value+1];
->>>>>>> origin/maku
             y_value++;
             Main.rooms.get(whereAmI).sizes[x_value][y_value] = 4;
 
@@ -34,15 +22,10 @@ public class Character {
         }
     }
 
-<<<<<<< HEAD
     void decreaseY() {
-        if (Main.rooms.get(whereAmI).sizes[x_value][y_value-1] == 1) {
-=======
-    public void decreaseY() {
         if (Main.rooms.get(whereAmI).sizes[x_value][y_value-1] != 2) {
             Main.rooms.get(whereAmI).sizes[x_value][y_value ] = last_tile;
             last_tile = Main.rooms.get(whereAmI).sizes[x_value][y_value-1];
->>>>>>> origin/maku
             y_value--;
             Main.rooms.get(whereAmI).sizes[x_value][y_value] = 4;
 
@@ -51,16 +34,10 @@ public class Character {
             whitchDoor(x_value, y_value-1);
         }
     }
-
-<<<<<<< HEAD
     void increaseX() {
-        if (Main.rooms.get(whereAmI).sizes[x_value+1][y_value] == 1) {
-=======
-    public void increaseX() {
         if (Main.rooms.get(whereAmI).sizes[x_value+1][y_value] != 2) {
             Main.rooms.get(whereAmI).sizes[x_value ][y_value] = last_tile;
             last_tile = Main.rooms.get(whereAmI).sizes[x_value+1][y_value];
->>>>>>> origin/maku
             x_value++;
             Main.rooms.get(whereAmI).sizes[x_value][y_value] = 5;
 
@@ -70,15 +47,10 @@ public class Character {
         }
     }
 
-<<<<<<< HEAD
     void decreaseX() {
-        if (Main.rooms.get(whereAmI).sizes[x_value-1][y_value] == 1) {
-=======
-    public void decreaseX() {
         if (Main.rooms.get(whereAmI).sizes[x_value-1][y_value] != 2) {
             Main.rooms.get(whereAmI).sizes[x_value ][y_value] = last_tile;
             last_tile = Main.rooms.get(whereAmI).sizes[x_value - 1][y_value];
->>>>>>> origin/maku
             x_value--;
             Main.rooms.get(whereAmI).sizes[x_value][y_value] = 4;
 
