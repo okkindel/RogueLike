eb5e0fdf3c25c7edc31c681285de33027e3c3d24
Maciej Hajduk <ebrithil97@gmail.com>
2017-06-25 13:19:15 +0200

    little bAttle fix

--- src/Battle.java
+++ src/Battle.java
@@ -9,14 +9,13 @@ public class Battle {
         int strength = generator.nextInt(enemy.strength_points);
         int defence = generator.nextInt(Character.defence_points) / 2;
         int true_damage = strength - defence;
-        if (true_damage <= 0) {
+        if (true_damage <= 0)
             Interface.newEvent("Character blocked attack.");
-        } else if (generator.nextInt(100) <= Character.dexterity_points) {
+        else if (generator.nextInt(100) <= Character.dexterity_points)
             Interface.newEvent("Character dodged attack.");
-        } else {
+        else
             Interface.newEvent(enemy.type + " attacks for : " + true_damage);
             Character.modifyHealth(-true_damage);
-        }
     }
 
     static void characterAttack(Enemies enemy) {
@@ -24,14 +23,13 @@ public class Battle {
         int strength = generator.nextInt(Character.strength_points);
         int defence = generator.nextInt(enemy.defence_points) / 2;
         int true_damage = strength - defence;
-        if (true_damage <= 0) {
+        if (true_damage <= 0)
             Interface.newEvent(enemy.type + " blocked attack.");
-        } else if (generator.nextInt(100) <= enemy.dexterity_points) {
+        else if (generator.nextInt(100) <= enemy.dexterity_points)
             Interface.newEvent(enemy.type + " dodged attack.");
-        } else {
+        else
             Interface.newEvent("Character attacks for : " + true_damage);
             enemy.health_points -= true_damage;
-        }
         if (enemy.health_points < 0)
             enemy.health_points = 0;
         else
